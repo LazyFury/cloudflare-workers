2524c4f8ca09da9a54da6eff9ecedbe136671427
suke <2568597007@qq.com>
2021-02-06 11:58:58 +0800

    handle err

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const qr = require('qr-image')
-const qs = require('qs')
 const querystring = require('querystring')
 
 addEventListener('fetch', event => {
@@ -10,17 +9,46 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  if (request.method == 'GET') {
-    try {
-      let url = querystring.parse(request.url.split('?')[1], '&', '=')
-      return new Response(qr.imageSync(url.text || ''), {
+  let url = new URL(request.url)
+  if (url.pathname == '/generator') {
+    if (request.method == 'GET') {
+      return await generatorQr(request)
+    }
+    return new Response('method not allowed', { status: 405 })
+  } else {
+    return new Response('route not found', { status: 404 })
+  }
+}
+
+/**
+ * 生成二维码
+ * @param {Request} request
+ */
+const generatorQr = request => {
+  let query = (request.url.split('?') || [])[1]
+  if (!query) {
+    return new Response('params is required', { status: 400 })
+  }
+  let url = querystring.parse(query || '', '&', '=')
+  if (!url.text) {
+    return new Response('params {text} is required', { status: 400 })
+  }
+  if (url.text.length > 300) {
+    return new Response('text is too long', { status: 414 })
+  }
+  if (url.text) {
+    return new Response(
+      qr.imageSync(url.text || '', {
+        type: 'png',
+        margin: 1,
+        parse_url: true,
+      }),
+      {
         status: 200,
         headers: { 'content-type': 'image/png' },
-      })
-    } catch (e) {
-      return new Response(e, { status: 500 })
-    }
+      },
+    )
+  } else {
+    return new Response('text can not be empty', { status: 400 })
   }
-
-  return new Response('method not allowed', { status: 405 })
 }
